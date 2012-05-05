c14f1a6fef0ab8bc81b393967e244cb5231240a7
Vivian McPhail <haskell.vivian.mcphail@gmail.com>
2012-05-05 18:32:18 +1200

    added Numeric.Statistics functions, part 1
    Ignore-this: b43c2b570684e57eb72eaf2708972614
    
    darcs-hash:20120505063218-af16d-7f819f9e036f11d4f6c60baaf7a451f712e55eda.gz

--- lib/Numeric/Statistics.hs
+++ lib/Numeric/Statistics.hs
@@ -15,9 +15,12 @@
 
 module Numeric.Statistics (
                            Sample,Samples
-                          , covarianceMatrix
+                          , covarianceMatrix, correlationCoefficientMatrix
                           , meanList, meanArray, meanMatrix
                           , varianceList, varianceArray, varianceMatrix
+                          --
+                          , centre, cloglog, cut, ranks, kendall, logit
+                          , mahanalobis
                           ) where
 
 
@@ -26,11 +29,14 @@ module Numeric.Statistics (
 --import Numeric.Vector
 --import Numeric.Matrix
 --import Numeric.Container
-import Numeric.LinearAlgebra
+import Numeric.LinearAlgebra hiding (rank)
 
 import qualified Data.Array.IArray as I 
 
+import qualified Data.Vector.Generic as GV
+
 import Numeric.GSL.Statistics
+import Numeric.GSL.Sort(sort)
 
 -----------------------------------------------------------------------------
 
@@ -45,6 +51,13 @@ covarianceMatrix :: Samples Double              -- ^ the dimensions of data (eac
 covarianceMatrix d = let (s,f) = I.bounds d
                       in fromArray2D $ I.array ((s,s),(f,f)) $ concat $ map (\(x,y) -> let c = covariance (d I.! x) (d I.! y) in if x == y then [((x,y),c)] else [((x,y),c),((y,x),c)]) $ filter (\(x,y) -> x <= y) $ I.range ((s,s),(f,f))
 
+-- | the correlation coefficient: (cov x y) / (std x) (std y)
+correlationCoefficientMatrix :: Samples Double -> Matrix Double
+correlationCoefficientMatrix d = let (s,f) = I.bounds d
+                           in fromArray2D $ I.array ((s,s),(f,f)) $ concat $ map (\(x,y) -> let { x' = d I.! x ; y' = d I.! y ; c = (covariance x' y') / ((stddev x') * (stddev y')) } in if x == y then [((x,y),c)] else [((x,y),c),((y,x),c)]) $ filter (\(x,y) -> x <= y) $ I.range ((s,s),(f,f))
+
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
 -----------------------------------------------------------------------------
 
 -- | the mean of a list of vectors
@@ -81,3 +94,82 @@ varianceMatrix :: (Container Vector a, Floating (Vector a), Element a) => Matrix
 varianceMatrix a = varianceList $ toRows a
 
 -----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+
+-- | centre the data to 0: (x - (mean x))
+centre :: Vector Double -> Vector Double
+centre v = v - (realToFrac (mean v))
+
+-----------------------------------------------------------------------------
+
+-- | complementary log-log function
+cloglog :: Vector Double -> Vector Double
+cloglog v = - log (- (log v))
+
+-----------------------------------------------------------------------------
+
+-- | cut numerical data into intervals, data must fall inside the bounds
+cut :: Vector Double 
+    -> Vector Double -- ^ intervals
+    -> Vector Int    -- ^ data indexed by bin
+cut v c  = let c' = sort c
+           in mapVector (\x -> cut_helper 0 x c') v 
+    where
+      cut_helper i x c 
+          | i >= dim c                       = error "Numeric.Statistics: cut: data point not within interval"
+          | x >= (c @> i) && x <= (c @> (i+1)) = i
+          | otherwise                       = cut_helper (i + 1) x c
+
+-----------------------------------------------------------------------------
+
+-- | return the rank of each element of the vector
+--     multiple identical entries result in the average rank of those entries
+ranks :: Vector Double -> Vector Double
+ranks v = let v' = sort v
+          in mapVector (\x -> 1 + rank_helper x v') v
+              where rank_helper x v' = let is = GV.elemIndices x v'
+                                       in (realToFrac (GV.foldl (+) 0 is)) / (fromIntegral $ dim is)
+
+-----------------------------------------------------------------------------
+
+-- | kendall's rank correlation τ
+kendall :: Vector Double -> Vector Double -> Matrix Double
+kendall x y = let ln = dim x
+                  rx = ranks x
+                  ry = ranks y
+                  r = fromColumns [rx,ry]
+                  m = signum $ (kronecker r (asColumn $ constant 1.0 ln)) - (kronecker (asRow $ constant 1.0 ln) r)
+                  c = rows m - 1
+              in correlationCoefficientMatrix $ I.listArray (0,c) (toColumns m)
+
+-----------------------------------------------------------------------------
+
+-- | (logit p) = log(p/(1-p))
+logit :: Vector Double -> Vector Double
+logit v =  mapVector (\x -> - (log ((1 / x) - 1))) v
+
+-----------------------------------------------------------------------------
+
+-- | the Mahalanobis D-square distance between samples
+--     columns are components and rows are observations
+mahalanobis :: Samples Double        -- ^ the data set
+            -> Maybe (Sample Double) -- ^ (Just sample) to be measured or use mean when Nothing
+            -> Double                -- ^ D^2 
+mahalanobis x u = let (_,xr) = I.bounds x
+                      xl     = I.elems x
+                      s'     = pinv $ covarianceMatrix x
+                      xu     = case u of
+                                 Nothing -> fromList $ map mean xl
+                                 Just m  -> m
+                      xm     = fromRows $ map ((-) xu) $ toRows $ fromColumns xl
+                      --um     = asColumn xu
+                      --w      = ((trans xm) <> xm + (trans um) <> um)/(fromIntegral $ xr - 1)
+                      --w'     = inv w
+                  in ((xm <> s' <> (trans xm)) @@> (0,0)) 
+
+m_test_samples :: Samples Double
+m_test_samples = I.listArray (1,3) $ map fromList [[1,2,3,4],[2,3,4,5],[1,3,2,4]]
+
+-----------------------------------------------------------------------------
+
