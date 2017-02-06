4b20ca0b1b5d0fe8b4a847fa729161f0bae8fd31
Vivian McPhail <haskell.vivian.mcphail@gmail.com>
2017-02-06 14:52:03 +1300

    change PCA to use SVD

--- lib/Numeric/Statistics/PCA.hs
+++ lib/Numeric/Statistics/PCA.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Numeric.Statistics.PCA
--- Copyright   :  (c) A. V. H. McPhail 2010, 2014
+-- Copyright   :  (c) A. V. H. McPhail 2010, 2014, 2017
 -- License     :  BSD3
 --
 -- Maintainer  :  haskell.vivian.mcphail <at> gmail <dot> com
@@ -27,38 +27,47 @@ import Numeric.LinearAlgebra
 
 import Numeric.GSL.Statistics
 
-import Numeric.Statistics
+--import Numeric.Statistics
 
 -----------------------------------------------------------------------------
 
 -- | find the principal components of multidimensional data greater than
 --    the threshhold
-pca :: I.Array Int (Vector Double)    -- the data
+pca :: I.Array Int (Vector Double)     -- the data
     -> Double                         -- eigenvalue threshold
-    -> Matrix Double
+    -> (Vector Double, Matrix Double) -- Eignevalues, Principal components
 pca d q = let d' = fmap (\x -> x - (scalar $ mean x)) d -- remove the mean from each dimension
-              cv = covarianceMatrix d'
-              (val',vec') = eigSH $ trustSym cv -- the covariance matrix is real symmetric
-              val = toList val'
-              vec = toColumns vec'
-              v' = zip val vec
+              d'' = fromColumns $ I.elems d'
+              (_,vec',uni') = svd d''
+              vec = toList vec'
+              uni = toColumns uni'
+              v' = zip vec uni
               v = filter (\(x,_) -> x > q) v'  -- keep only eigens > than parameter
-          in fromColumns $ snd $ unzip v
+              (eigs,vs) = unzip v
+          in (fromList eigs,fromColumns vs) 
 
 -- | find N greatest principal components of multidimensional data
 --    according to size of the eigenvalue
 pcaN :: I.Array Int (Vector Double)    -- the data
      -> Int                            -- number of components to return
-     -> Matrix Double
+     -> (Vector Double, Matrix Double) -- Eignevalues, Principal components
 pcaN d n = let d' = fmap (\x -> x - (scalar $ mean x)) d -- remove the mean from each dimension
-               cv = covarianceMatrix d'
-               (val',vec') = eigSH $ trustSym cv  -- the covariance matrix is real symmetric
-               val = toList val'
-               vec = toColumns vec'
-               v' = zip val vec
+               d'' = fromColumns $ I.elems d'
+               (_,vec',uni') = svd d''
+               vec = toList vec'
+               uni = toColumns uni'
+               v' = zip vec uni
                v = take n $ reverse $ sortBy (comparing fst) v'
-           in fromColumns $ snd $ unzip v
+               (eigs,vs) = unzip v
+           in (fromList eigs,fromColumns vs) 
 
+v1 = fromList [1,2,3,4,5,6::Double]
+v2 = fromList [2,3,4,5,6,7::Double]
+v3 = fromList [3,4,5,6,7,8::Double]
+
+a = fromColumns [v1,v2,v3]
+b = I.listArray (1,3::Int) [v1,v2,v3] :: I.Array Int (Vector Double)
+                
 -- | perform a PCA transform of the original data (remove mean)
 -- |     Final = M^T Data^T
 pcaTransform :: I.Array Int (Vector Double)    -- ^ the data
@@ -74,11 +83,11 @@ pcaReduce :: I.Array Int (Vector Double)      -- ^ the data
           -> I.Array Int (Vector Double)      -- ^ the reduced data
 pcaReduce d q = let u = fmap (scalar . mean) d
                     d' = zipWith (-) (I.elems d) (I.elems u)
-                    cv = covarianceMatrix $ I.listArray (I.bounds d) d'
-                    (val',vec') = eigSH $ trustSym cv -- the covariance matrix is real symmetric
-                    val = toList val'
-                    vec = toColumns vec'
-                    v' = zip val vec
+                    d'' = fromColumns d'
+                    (_,vec',uni') = svd d''
+                    vec = toList vec'
+                    uni = toColumns uni'
+                    v' = zip vec uni
                     v = filter (\(x,_) -> x > q) v'  -- keep only eigens > than parameter
                     m = fromColumns $ snd $ unzip v
                  in I.listArray (I.bounds d) $ zipWith (+) (toRows $ m <> (tr' m) <> fromRows d') (I.elems u) 
@@ -89,11 +98,11 @@ pcaReduceN :: I.Array Int (Vector Double)      -- ^ the data
            -> I.Array Int (Vector Double)      -- ^ the reduced data, with n principal components
 pcaReduceN d n = let u = fmap (scalar . mean) d
                      d' = zipWith (-) (I.elems d) (I.elems u)
-                     cv = covarianceMatrix $ I.listArray (I.bounds d) d'
-                     (val',vec') = eigSH $ trustSym cv -- the covariance matrix is real symmetric
-                     val = toList val'
-                     vec = toColumns vec'
-                     v' = zip val vec
+                     d'' = fromColumns d'
+                     (_,vec',uni') = svd d''
+                     vec = toList vec'
+                     uni = toColumns uni'
+                     v' = zip vec uni
                      v = take n $ reverse $ sortBy (comparing fst) v'
                      m = fromColumns $ snd $ unzip v
                   in I.listArray (I.bounds d) $ zipWith (+) (toRows $ m <> (tr' m) <> fromRows d') (I.elems u) 
